a045593f577afaf24567bc623e93651aad5baa58
Daniel <d.harrison.lima@gmail.com>
2024-06-06 12:46:40 -0300

    ambiente configurado

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
 import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
+app.get('/hello', () => {
+  return 'Hello World'
+})
 
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('http server running')
+  })
+//
