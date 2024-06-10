9d221a4149dd264a282aba4c9ca82088b552c5f5
Daniel <d.harrison.lima@gmail.com>
2024-06-10 19:06:07 -0300

    test transaction route

---
+++ test/example.spec.ts
@@ -0,0 +1,24 @@
+import { expect, test, beforeAll, afterAll } from 'vitest'
+import resquest from 'supertest'
+import { app } from '../src/app'
+
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+test('User can create a new transaction', async () => {
+  await resquest(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
+
+  // expect(response.statusCode).toEqual(201)
+})
